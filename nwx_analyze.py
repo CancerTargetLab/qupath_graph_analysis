b474ce25ad1807e16a232482711b28e16b3c1784
Mattis Knulst <mattis.knulst@immun.lth.se>
2022-09-26 08:48:38 +0200

    split main function into grap gen, plotting and statistics

--- nwx_analyze.py
+++ nwx_analyze.py
@@ -34,32 +34,17 @@ parser.add_argument('-t', '--tiff_dir', type=str, help='path to the tiff directo
 
 args = parser.parse_args()
 
-
-def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend='',
-                 pair='CD45:PANCK'):
+def make_graph(df, critical_distance):
     """
-    This function subsets images from a dataframe and plots the network graph as well as the corresponding aac
+    Make a graph from a dataframe with x, y coordinates and cell type
+    :param df: dataframe with x, y coordinates and cell type (qupath output)
+    :param critical_distance: distance in pixels
+    :return: graph
     """
-    cell_type_filter = pair.split(':')
-    #class_1 = df.groupby('Class').get_group(cell_type_filter[0])
-    #class_2 = df.groupby('Class').get_group(cell_type_filter[1])
-    # get image file path
-    image_file = tiff_dir + '/' + image
-    # get image
-    my_img = plt.imread(image_file)
-    filtered = df[(df['Class'] == cell_type_filter[0]) | (df['Class'] == cell_type_filter[1])].reset_index(drop=True)
-    # we don't want colons in file names
-    nice = pair.replace(':','_')
-    out_file = f'{nice}_{prepend}image_{image}_distance_{critical_distance}.png'
-    # extract the image
-    one_pic = filtered.groupby('Image').get_group(image).reset_index(drop=True)
-
     # extract the spatial coordinates
-    coordinates = one_pic.iloc[:, [5, 6]]
+    coordinates = df.iloc[:, [5, 6]]
     # extract the cell types
-    cell_types = one_pic.Class
-
-
+    cell_types = df.Class
     # Creating a graph from coordinates
     positions =  (coordinates.to_numpy())
 
@@ -69,11 +54,6 @@ def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend=''
     # create a networkx graph from the weights object
     G = nx.from_numpy_matrix(w.full()[0])
 
-    # calculate euclidean distance
-    # dist = distance.pdist(positions)
-    # create a networkx graph from the distance matrix
-    # G = nx.from_numpy_matrix(distance.squareform(dist))
-
     # add the coordinates to the graph
     for i, (x, y) in enumerate(positions):
         G.nodes[i]['pos'] = (x, y)
@@ -82,14 +62,33 @@ def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend=''
     for i, cell_type in enumerate(cell_types):
         G.nodes[i]['cell_type'] = cell_type
 
-    # Plotting the graph
-    aac = nx.attribute_assortativity_coefficient(G, 'cell_type')
+
+    return G, w
+
+def plot_graph(G, pair, results_dir, cell_type_filter, image_file, critical_distance, prepend=''):
+    """
+    This function plots the graph
+    :param G: graph
+    :param pair: cell type pair
+    :param results_dir: path to the results directory
+    :param cell_type_filter: cell type filter
+    :param image_file: path to the image file
+    :param critical_distance: critical distance
+    :param prepend: string to prepend to the filename
+    :return: None
+    """
+    # get image
+    my_img = plt.imread(image_file)
+    # we don't want colons in file names
+    nice = pair.replace(':','_')
+    out_file = f'{nice}_{prepend}image_{image}_distance_{critical_distance}.png'
+    cell_types = [cell_type for node, cell_type in G.nodes(data='cell_type')]
+    # plot the graph
     fig = plt.figure(figsize=(10, 10))
     y_lim, x_lim = my_img.shape[0], my_img.shape[1]
     extent = 0, x_lim, 0, y_lim
     # transpose image
-    # my_img = np.transpose(my_img, (1, 0, 2))
-    # my_img = np.fliplr(my_img)
+
     my_img = np.flipud(my_img)
     # draw the image
     plt.imshow(my_img, cmap='gray', extent=extent, interpolation='nearest')
@@ -104,13 +103,24 @@ def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend=''
                      edge_color='yellow', width=0.5, with_labels=False)
 
 
-    plt.title(f'{c1} {c2} image {image} distance {critical_distance}px aac {aac:.2f}')
+    plt.title(f'{c1} {c2} image {image} distance {critical_distance}px')
 
     plt.savefig(f'{results_dir}/{out_file}')
 
+
+def calculate_statistics(df, G, w, cell_type_filter):
+    """
+    This function calculates the statistics
+    :param df: dataframe with x, y coordinates and cell type (qupath output)
+    :param G: graph
+    :param w: weights object
+    :param cell_type_filter: cell type filter
+    :return: tuple of statistics
+    """
+    aac = nx.attribute_assortativity_coefficient(G, 'cell_type')
     # count nr of cells in each class
-    n_cells_class_1 = one_pic.loc[one_pic['Class'] == cell_type_filter[0], 'Class'].count()
-    n_cells_class_2 = one_pic.loc[one_pic['Class'] == cell_type_filter[1], 'Class'].count()
+    n_cells_class_1 = df.loc[df['Class'] == cell_type_filter[0], 'Class'].count()
+    n_cells_class_2 = df.loc[df['Class'] == cell_type_filter[1], 'Class'].count()
     # count number of islands
     n_islands = len(w.islands)
     # get all nodes belonging to class 1
@@ -124,6 +134,31 @@ def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend=''
         centrality_measures = 'NA'
     # ratio is the proportion of class 1 cells in the network
     ratio = n_cells_class_1 / (n_cells_class_2 + n_cells_class_1)
+
+    return aac, n_cells_class_1, n_cells_class_2, n_islands, centrality_measures, ratio
+
+
+def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend='',
+                 pair='CD45:PANCK'):
+    """
+    main function for iterating image by image
+    """
+    cell_type_filter = pair.split(':')
+    #class_1 = df.groupby('Class').get_group(cell_type_filter[0])
+    #class_2 = df.groupby('Class').get_group(cell_type_filter[1])
+    # get image file path
+    image_file = tiff_dir + '/' + image
+    filtered = df[(df['Class'] == cell_type_filter[0]) | (df['Class'] == cell_type_filter[1])].reset_index(drop=True)
+    # extract the image
+    one_pic = filtered.groupby('Image').get_group(image).reset_index(drop=True)
+    # make graph
+    G, w = make_graph(one_pic, critical_distance)
+    # Plotting the graph
+    plot_graph(G, pair, results_dir, cell_type_filter, image_file, critical_distance, prepend=prepend)
+    # calculate statistics
+    aac, n_cells_class_1, n_cells_class_2, n_islands, centrality_measures, ratio = calculate_statistics(one_pic, G, w, cell_type_filter)
+
+
     return pd.Series({'image': image,
             'class_1': cell_type_filter[0],
             'class_2': cell_type_filter[1],
@@ -133,6 +168,9 @@ def network_plot(df, image, tiff_dir, critical_distance, results_dir, prepend=''
             'n_cells_class_1': n_cells_class_1,
             'n_cells_class_2': n_cells_class_2,
             'n_islands': n_islands})
+
+
+
 
 if __name__ == "__main__":
     # create the output dictionary
